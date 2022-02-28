1b04d6ab3a853f3a6728d23b71ce40a31d118eca
unknown <libenciuc.adrian@yahoo.com>
2022-02-28 21:16:27 +0200

    MainUI developed

--- src/main/java/com/facebook/Application.java
+++ src/main/java/com/facebook/Application.java
@@ -1,7 +1,15 @@
 package com.facebook;
 
+import com.facebook.ui.MainUI;
+
 public class Application {
+
+    static MainUI mainUI = new MainUI();
+
     public static void main(String[] args) {
 
+        mainUI.showMainUI();
+
     }
+
 }
