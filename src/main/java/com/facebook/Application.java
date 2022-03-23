259709f034948c10962bcd8d1fb0d9e2c01d5a41
Mitrea Teofil <teofil.mitrea@yahoo.com>
2022-03-23 21:45:11 +0200

    Radu, Teofil, Radu

--- src/main/java/com/facebook/Application.java
+++ src/main/java/com/facebook/Application.java
@@ -1,16 +1,5 @@
 package com.facebook;
 
-<<<<<<< HEAD
-import com.facebook.model.HomePage;
-
-import java.io.IOException;
-
-public class Application {
-    public static void main(String[] args) throws IOException {
-
-        HomePage homePage = new HomePage();
-        homePage.newsFeed();
-=======
 import com.facebook.ui.MainUI;
 
 import java.awt.*;
@@ -23,7 +12,6 @@ public class Application {
     public static void main(String[] args) throws AWTException, InterruptedException, IOException {
 
         mainUI.showMainUI();
->>>>>>> main
 
     }
 
